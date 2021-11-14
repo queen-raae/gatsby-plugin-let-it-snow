735881dad19b3e50df250683f78e90ba45e57ec2
pauliescanlon <paul.scanlon@gatsbyjs.com>
2021-11-14 11:34:57 +0000

    feat: move pluginOptionsSchema to gatsby-node

--- plugin/gatsby-node.js
+++ plugin/gatsby-node.js
@@ -1,1 +1,8 @@
 // https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
+export const pluginOptionsSchema = ({ Joi }) => {
+  return Joi.object({
+    colors: Joi.array()
+      .items(Joi.string())
+      .description("Array of hex color values"),
+  });
+};
