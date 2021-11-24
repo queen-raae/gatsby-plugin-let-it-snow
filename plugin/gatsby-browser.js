f4f3d698736eead16babb725765b096f9a2cedf6
inezabonte <inezabonte@gmail.com>
2021-11-24 09:19:59 +0200

    linting

--- plugin/gatsby-browser.js
+++ plugin/gatsby-browser.js
@@ -3,7 +3,7 @@
 import confetti from "canvas-confetti";
 
 export const onInitialClientRender = (_, options) => {
-  const { colors = ["#ffffff"], intensity } = options
+  const { colors = ["#ffffff"], intensity } = options;
 
   const duration = 15 * 1000;
   const animationEnd = Date.now() + duration;
@@ -18,23 +18,22 @@ export const onInitialClientRender = (_, options) => {
     const ticks = Math.max(200, 500 * (timeLeft / duration));
     skew = Math.max(0.8, skew - 0.001);
 
-    const intensityValues= {
+    const intensityValues = {
       startVelocity: 0,
       gravity: randomInRange(0.4, 0.6),
       scalar: randomInRange(0.4, 1),
       drift: randomInRange(-0.4, 0.4),
-    }
-  
-    if ( intensity === 'blizzard' ) {
+    };
+
+    if (intensity === "blizzard") {
       intensityValues.gravity = randomInRange(0.4, 5);
-      intensityValues.drift =  randomInRange(-0.4, 20);
+      intensityValues.drift = randomInRange(-0.4, 20);
     }
-  
-    if ( intensity === 'light-snow' ) {
+
+    if (intensity === "light-snow") {
       intensityValues.gravity = randomInRange(0.1, 0.2);
       intensityValues.scalar = randomInRange(0.4, 0.6);
-      intensityValues.drift =  randomInRange(0, 0);
-  
+      intensityValues.drift = randomInRange(0, 0);
     }
 
     confetti({
