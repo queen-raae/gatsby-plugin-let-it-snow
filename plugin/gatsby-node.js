c220671a7e8cbfacfcca55991cb5a70a8416728b
Ola <ola@olavea.com>
2021-11-25 08:39:58 +0100

    fix: one step further, more work InTheMorning

--- plugin/gatsby-node.js
+++ plugin/gatsby-node.js
@@ -4,17 +4,9 @@ exports.pluginOptionsSchema = ({ Joi }) => {
     colors: Joi.array()
       .items(Joi.string().default("#ffffff"))
       .description("Array of hex color values"),
-  });
-};
-
-exports.pluginOptionsSchema = ({ Joi }) => {
-  return Joi.object({
-    // Validate that the anonymize option is defined by the user and is a boolean
     duration: Joi.array()
       .items(Joi.number().default(15 * 1000))
       .description("Pick a number"),
-  })
-}
+  });
+};
 
-// ().default("15"))
-//       .description("Array of hex color values"),
