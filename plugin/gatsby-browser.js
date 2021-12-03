queen-raae/gatsby-plugin-let-it-snow

commit 9319fa0c8685ce4db85ec1af4d0419b263e2ec92

feat: season-plugin-option, solves #2 (#11)
* feat: add season start and end

* feat: add pluginOptionsSchema for season

* chore: update README with time included in date string

* chore: blast - add seconds to end time string

* chore: test max time in seconds

* chore: add date-fns package

* docs: update readme

* fix: better defaults

* feat: only do snow when in season

* chore: full config of plugin in demo

* chore: only rely on schema defaults

* fix: catch all possible season errors

Co-authored-by: Benedicte Raae <raae@raae.codes>

plugin/gatsby-browser.js
@@ -1,12 +1,51 @@
 // https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
 
 import confetti from "canvas-confetti";
+import {
+  addYears,
+  getYear,
+  setYear,
+  isBefore,
+  isWithinInterval,
+  parseISO,
+} from "date-fns";
+
+const isSeason = ({ start, end }) => {
+  try {
+    const currentDate = new Date();
+    const currentYear = getYear(currentDate);
+
+    // Ignore year from config dates
+    const startDate = setYear(parseISO(start), currentYear);
+    let endDate = setYear(parseISO(end), currentYear);
+
+    if (isBefore(endDate, startDate)) {
+      endDate = addYears(endDate, 1);
+    }
+
+    return isWithinInterval(currentDate, {
+      start: startDate,
+      end: endDate,
+    });
+  } catch (error) {
+    console.warn(
+      "Problem with @raae/gatsby-plugin-let-it-snow season configuration:",
+      error.message
+    );
+    return false;
+  }
+};
 
 export const onInitialClientRender = (_, options) => {
-  const { colors = ["#ffffff"], intensity } = options;
+  const { colors, intensity, season } = options;
 
+  if (!isSeason(season)) {
+    return;
+  }
+
+  const now = Date.now();
   const duration = 15 * 1000;
-  const animationEnd = Date.now() + duration;
+  const animationEnd = now + duration;
   let skew = 1;
 
   function randomInRange(min, max) {
@@ -14,7 +53,7 @@ export const onInitialClientRender = (_, options) => {
   }
 
   const frame = () => {
-    const timeLeft = animationEnd - Date.now();
+    const timeLeft = animationEnd - now;
     const ticks = Math.max(200, 500 * (timeLeft / duration));
     skew = Math.max(0.8, skew - 0.001);
 

demo/gatsby-config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  plugins: ["@raae/gatsby-plugin-let-it-snow"],
+  plugins: [
+    {
+      resolve: "@raae/gatsby-plugin-let-it-snow",
+      options: {
+        colors: ["#fff", "#ffb6c1"],
+        season: {
+          start: new Date("December 2, 2023"),
+          end: new Date("January 11, 2024"),
+        },
+      },
+    },
+  ],
 };

plugin/gatsby-node.js
@@ -2,10 +2,22 @@
 exports.pluginOptionsSchema = ({ Joi }) => {
   return Joi.object({
     colors: Joi.array()
-      .items(Joi.string().default("#ffffff"))
+      .items(Joi.string())
+      .min(1)
+      .default(["#fff"])
       .description("Array of hex color values"),
     intensity: Joi.string()
       .valid("regular", "light", "blizzard")
       .default("regular"),
+    season: Joi.object()
+      .keys({
+        start: Joi.date().required(),
+        end: Joi.date().required(),
+      })
+      .default({
+        start: new Date("December 1"),
+        end: new Date("January 4"),
+      })
+      .description("Start and end date for when snow should be activated"),
   });
 };

commit 616a01f1cbadca3bd7d39e7e92bc01f5c157c036

fix: duration broken by #11

plugin/gatsby-browser.js
@@ -43,9 +43,9 @@ export const onInitialClientRender = (_, options) => {
     return;
   }
 
-  const now = Date.now();
   const duration = 15 * 1000;
-  const animationEnd = now + duration;
+
+  const animationEnd = Date.now() + duration;
   let skew = 1;
 
   function randomInRange(min, max) {
@@ -53,7 +53,7 @@ export const onInitialClientRender = (_, options) => {
   }
 
   const frame = () => {
-    const timeLeft = animationEnd - now;
+    const timeLeft = animationEnd - Date.now();
     const ticks = Math.max(200, 500 * (timeLeft / duration));
     skew = Math.max(0.8, skew - 0.001);
 