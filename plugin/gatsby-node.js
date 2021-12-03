ed088a777a7066a07bf95b39c41faa7e26f5a9c7
Benedicte Raae <raae@raae.codes>
2021-12-03 13:53:03 +0100

    fix: better defaults

--- plugin/gatsby-node.js
+++ plugin/gatsby-node.js
@@ -2,12 +2,17 @@
 exports.pluginOptionsSchema = ({ Joi }) => {
   return Joi.object({
     colors: Joi.array()
-      .items(Joi.string().default("#ffffff"))
+      .items(Joi.string())
+      .default(["#fff"])
       .description("Array of hex color values"),
     season: Joi.object()
       .keys({
         start: Joi.date(),
         end: Joi.date(),
+      })
+      .default({
+        start: new Date("December 1"),
+        end: new Date("January 4"),
       })
       .description("Start and end date for when snow should be visible"),
   });
