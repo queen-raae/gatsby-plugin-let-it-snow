c20b0cba0ed42df4f1a868575c5fdbc06bf40846
Benedicte Raae <raae@raae.codes>
2021-12-06 21:58:33 +0100

    demo: longer duration to test change

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
       resolve: "@raae/gatsby-plugin-let-it-snow",
       options: {
         colors: ["#EBDEF0", "--snow1", "--snow2", "--snow3"],
-        duration: 18,
+        duration: 30,
         season: {
           start: new Date("December 2, 2023"),
           end: new Date("January 11, 2024"),
