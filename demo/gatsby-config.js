233894174d172d90ea9c54dabadd1330ca2b8ae4
Benedicte Raae <raae@raae.codes>
2022-02-08 11:42:53 +0100

    demo: snow all year

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -6,8 +6,8 @@ module.exports = {
         colors: ["#EBDEF0", "--snow1", "--snow2", "--snow3"],
         duration: 30,
         season: {
-          start: new Date("December 2, 2023"),
-          end: new Date("January 11, 2024"),
+          start: new Date("January 1"),
+          end: new Date("December 31"),
         },
       },
     },
